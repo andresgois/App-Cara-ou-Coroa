6c49078029e458c400c8af8ff931fb80e5f5ecfc
Andre gois <andre.s.gois3@gmail.com>
2021-12-11 18:12:23 -0300

    adding scrollview to the page over and shaping the text

--- src/components/Sobre/Sobre.js
+++ src/components/Sobre/Sobre.js
@@ -1,15 +1,35 @@
 
 import React, { Component } from 'react';
-import { StyleSheet, Text,  View } from 'react-native';
+import { ScrollView, StyleSheet, Text,  View } from 'react-native';
 
 export default class App extends Component {
     render(){
       return(
-        <View style={ styles.container}>
-          <Text>
-          A expressão Lorem ipsum em design gráfico e editoração é um texto padrão em latim utilizado na produção gráfica para preencher os espaços de texto em publicações para testar e ajustar aspectos visuais antes de utilizar conteúdo real.
+        <ScrollView style={ styles.container}>
+          <Text style={ [styles.texto, {fontWeight: 'bold', fontSize: 30, textAlign: 'center'}]}>
+              What is Lorem Ipsum?
           </Text>
-        </View>
+          <Text style={ styles.texto}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type 
+            specimen book. 
+          </Text>
+          <Text style={ styles.texto}>
+            It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in 
+            the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including 
+            versions of Lorem Ipsum.
+          </Text>
+          <Text style={ styles.texto}>
+            It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in 
+            the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including 
+            versions of Lorem Ipsum.
+          </Text>
+        </ScrollView>
       )
     }
 };
@@ -19,5 +39,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#61BD8C',
   },
+  texto:{
+    padding: 15,
+    color: '#fff',
+    fontSize: 23,
+  }
 });
 
