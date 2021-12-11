5b44df2fa2bc171b3a9146ab864b48c9d49fd721
Andre gois <andre.s.gois3@gmail.com>
2021-12-11 18:33:41 -0300

    component makeover others

--- src/components/Outros/Outros.js
+++ src/components/Outros/Outros.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { Image, StyleSheet, Text,  View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text,  View } from 'react-native';
 
 const logo = require('./src/imgs/logo.png');
 
@@ -8,9 +8,22 @@ export default class App extends Component {
     render(){
       return(
         <View style={ styles.container}>
-          <Text>
-            Outros Jogos
+          <Text style={[styles.texto, {fontWeight: 'bold', fontSize: 30, textAlign: 'center'}]}>
+            Jogos
           </Text>
+          <View>
+            <FlatList 
+              data={[
+                {key: '> Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {key: '> Nam feugiat sem vitae ante feugiat tincidunt.'},
+                {key: '> Maecenas venenatis odio in orci ullamcorper, vel tempus elit iaculis.'},
+                {key: '> In euismod ante nec metus tristique, imperdiet aliquet libero congue.'},
+                {key: '> Aliquam non nibh dapibus neque posuere pharetra.'},
+                {key: '> Phasellus ut odio ac nisl imperdiet imperdiet eu vel tellus.'},
+              ]}
+              renderItem={({item}) => <Text style={styles.texto}> {item.key}</Text>}
+            />
+          </View>
         </View>
       )
     }
@@ -21,5 +34,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#61BD8C',
   },
+  texto:{
+    padding: 10,
+    color: '#fff',
+    fontSize: 18,
+  }
 });
 
