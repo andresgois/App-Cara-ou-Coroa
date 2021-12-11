5ecb55e5c332411a439474dbca00062f52f0e6e2
Andre gois <andre.s.gois3@gmail.com>
2021-12-11 18:05:40 -0300

    putting style in the navbar

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Router, Scene, Stack, Actions } from 'react-native-router-flux';
+import { color } from 'react-native-reanimated';
+import { Router, Scene, Actions } from 'react-native-router-flux';
 import Principal from '../Main/Principal';
 import Outros from '../Outros/Outros';
 import Resultado from '../Resultado/Resultado';
@@ -9,31 +10,37 @@ class Route extends Component {
   render(){
     return (
       <Router sceneStyle={{ paddingTop: 50}}>
-        <Stack key='root'>
-          <Scene key='principal' component={ Principal } initial title="Jogo Cara ou Coroa" />
+        <Scene key="root">
+          <Scene 
+            key='principal' 
+            component={ Principal } 
+            initial 
+            title="Jogo Cara ou Coroa" 
+            navigationBarStyle={{backgroundColor: '#274c38'}}
+            titleStyle={{ color: '#fff', textAlign: 'center', fontWeight: 'bold', fontSize: 28}}
+          />
 
           <Scene key='outros' component={ Outros } title="Outros Jogos" 
-            leftTitle="<"
-            onLeft={() => {
-              Actions.pop();
-            }}
+            back
+            navBarButtonColor='#61BD8C'             
+            titleStyle={{color: '#61BD8C', textAlign: 'center', fontWeight: 'bold', fontSize: 28}}
           />
 
           <Scene key='sobre' component={ Sobre } title="Sobre o Jogo"             
-            leftTitle="<"
-            onLeft={() => {
-              Actions.pop();
-            }}
+            back
+            navBarButtonColor='#61BD8C'             
+            titleStyle={{color: '#61BD8C', textAlign: 'center', fontWeight: 'bold', fontSize: 28}}
           />
 
-          <Scene key='resultado' component={ Resultado } title="Resultado"
-            leftTitle="<"
-            onLeft={() => {
-              Actions.pop();
-            }}
+          <Scene 
+            key='resultado' 
+            component={ Resultado } 
+            title="Resultado"
+            back
+            navBarButtonColor='#61BD8C'             
+            titleStyle={{color: '#61BD8C', textAlign: 'center', fontWeight: 'bold', fontSize: 28}}
           />
-
-        </Stack>
+        </Scene>
       </Router>
     );
   }
